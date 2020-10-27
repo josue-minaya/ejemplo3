cabef3dc8ead4dd321e888f88bbdd9494b93c3ec
Josue Minaya <josuemin_17outlook.com>
2020-10-27 17:14:09 -0500

    l

---
+++ ejemplo3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ejemplo3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
